4e6fe2e607060486f01147a7a61cf09b96059655
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2025-08-02 11:16:37 +0545

    added blog and auth route handlers

---
+++ server/src/routes/blogRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { createBlog, getBlogs, getBlogById, updateBlog, deleteBlog, addComment } from '../controllers/blogController';
+import { protect } from '../middleware/authmiddleware';
+
+const router = express.Router();
+
+router.get('/', getBlogs);
+router.get('/:id', getBlogById);
+router.post('/', protect, createBlog);
+router.put('/:id', protect, updateBlog);
+router.delete('/:id', protect, deleteBlog);
+router.post('/:id/comment', protect, addComment);
+
+export default router;
